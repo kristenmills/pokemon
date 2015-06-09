050e9462b1ac3b2994af718fb94a698ff1d7af99
Kristen Mills <kristen@kristen-mills.com>
2015-06-09 10:41:39 -0400

    Converted index.js to user koa. Might work...might not

--- server/index.js
+++ server/index.js
@@ -1,49 +1,38 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var jwt = require('express-jwt');
+var koa = require('koa');
+var bodyParser = require('koa-body');
+var logger = require('koa-logger');
+var cors = require('koa-cors');
+var jwt = require('koa-jwt');
 
 var env = process.env.NODE_ENV || 'development';
 
-var app = module.exports = express();
+var app = module.exports = koa();
 
 // var keys = require('./helpers/keys');
 
 app.use(cors());
 // app.use(jwt({algorithms: ['RS256','RS384','RS512' ], secret: keys.pub}).unless({ path: ['/api/token'] }));
 if(env === 'development') {
-  app.use(logger('dev'));
+  app.use(logger());
 }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use(function(req, res, next) {
-  req.models = app.models;
-  next();
-});
+app.use(bodyParser());
 
 require('./routes')();
 
 // error handlers
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-app.use(function(err, req, res, next) {
-  if (env === 'production') {
-    res.status(err.status || 500).send('internal server error!');
-  }
-  else {
-    if(env === 'development' && err.stack) {
-      console.error(err.stack);
+app.use(function *errors(next) {
+  try {
+    yield next;
+  } catch (err) {
+    this.status = err.status || 500;
+    if(env === 'production') {
+      this.body = 'Internal Server Error';
+    } else {
+      this.body = err;
     }
-    var status = err.status;
-    err.status = undefined;
-    res.status(status || 500).send(err);
+    this.app.emit('error', err, this);
   }
 });
 
