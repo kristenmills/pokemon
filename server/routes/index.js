e0e61608eaeaaa3cf1185301b13700f71ceb2a7c
Kristen Mills <kristen@kristen-mills.com>
2015-06-09 10:51:49 -0400

    Updated server/routes/index.js. Again could work...could not work

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,15 @@
 var path = require('path');
+var sendFile = require('koa-send');
 
 var app = require('..');
 
 module.exports = function() {
   app.use('/api/cards', require('./cards'));
-  app.use(function(req, res, next) {
-    res.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html'));
+  app.use(function *(next) {
+    if(this.is('html')){
+      yield sendFile(this, path.join(__dirname, '..', '..', 'dist', 'index.html'));
+    } else {
+      yield next;
+    }
   });
 }
